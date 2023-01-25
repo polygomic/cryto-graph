25f56afe027a238ef1564cd0e5068422895cd3ef
git <eae966@gmail.com>
2023-01-25 15:22:59 +0330

    Other bug fixes.

--- src/exchange/pricing.ts
+++ src/exchange/pricing.ts
@@ -74,7 +74,7 @@ export function findCrytoPerToken(token: Token): BigDecimal {
   }
   // loop through whitelist and check if paired with any
   for (let i = 0; i < WHITELIST.length; ++i) {
-    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]));
+    let pairAddress = factoryContract.try_getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]));
     if (pairAddress.toHexString() != ADDRESS_ZERO) {
       if (WHITELISTPAIRS.includes(pairAddress.toHexString())) {
         let pair = Pair.load(pairAddress.toHexString());
